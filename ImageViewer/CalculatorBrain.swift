b341ef40cbc60c6cf15d99c91f890b163c50d2e2
Rob Ranf <robranf@gmail.com>
2020-09-25 14:42:41 -0700

    adding detail view controller

---
+++ ImageViewer/CalculatorBrain.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Rob Ranf on 9/24/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
--- ImageViewer/ViewController.swift
+++ ImageViewer/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var images = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                images.append(item)
+            }
+        }
+        print(images)
     }
-
-
+    // Remember, _ is the external name of the first param, tableView is the internal name, and UITableView is the type; same for the second param: numberOfRowsInSection is external, section is internal, and Int is type
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = images[indexPath.row]
+        return cell
+    }
 }
 
