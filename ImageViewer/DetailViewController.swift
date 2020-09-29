0f00c711e2ff676ff9568f78111ed1c13d568a8a
Rob Ranf <robranf@gmail.com>
2020-09-29 16:04:55 -0700

    Completing navbar config

--- ImageViewer/DetailViewController.swift
+++ ImageViewer/DetailViewController.swift
@@ -14,6 +14,8 @@ class DetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
 
         // Do any additional setup after loading the view.
         // First we check and unwap the selectedImage optional
@@ -21,6 +23,16 @@ class DetailViewController: UIViewController {
         if let imageToLoad = selectedImage {
             imageView.image = UIImage(named: imageToLoad)
         }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
     }
     
 
